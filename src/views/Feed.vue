d86f3746cccc55f5b1fe4f47e50280fc64403715
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2023-01-16 10:51:36 -0600

    added functional search bars

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -216,12 +216,66 @@
       </div>
     </div>
     <!-- Cards -->
+    <!-- Search Bar -->
+    <form class="flex items-center mx-4 my-2">
+      <label for="simple-search" class="sr-only">Search</label>
+      <div class="relative w-full">
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="simple-search"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search for Recipes..."
+          v-model="search"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        class="p-2.5 ml-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-white hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-white"
+      >
+        <svg
+          class="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          ></path>
+        </svg>
+        <span class="sr-only">Search</span>
+      </button>
+    </form>
+
+    <div class="divider"></div> 
+
     <div class="px-4 mb-10 grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4">
       <div
-        v-for="recipe in recipes"
+        v-for="recipe in filteredList"
         class="bg-tan card card-compact text-black hover:bg-coconut hover:text-white hover:-translate-y-6 hover:-translate-x-6 transition duration-700 w-full h-full lg:w-80 md:w-60 sm:w-52 w-42 mb-24 shadow-xl mx-auto"
       >
-      <span class="material-icons text-center text-9xl"> menu_book </span>
+        <span class="material-icons text-center text-9xl"> menu_book </span>
         <div class="card-body">
           <p class="text-center font-bold sm:text-lg text-sm">
             {{ recipe.recipeTitle }}
@@ -283,6 +337,7 @@ export default {
       groceryStats: [],
       isLoading: false,
       index: 0,
+      search: "",
       addRecipeForm: false,
     };
   },
@@ -301,6 +356,13 @@ export default {
     },
     FIBTG() {
       return this.groceryStats.parsed[this.index].food.nutrients.FIBTG;
+    },
+    filteredList() {
+      return this.recipes.filter((recipe) => {
+        return recipe.recipeTitle
+          .toLowerCase()
+          .includes(this.search.toLowerCase());
+      });
     },
   },
   methods: {
